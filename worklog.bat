4b792eb6bdfe71ea549c06a15eabac10dae72af6
Igor Oliveira <igor.olivrd@gmail.com>
2026-01-08 16:56:38 -0300

    Add files via upload

---
+++ worklog.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Altera o diretório atual para o local do script Python
+cd C:\Users\igor.oliveira\Documents\PROJETOS - BI\API_WL_JIRA
+REM Executa o script Python
+python app.py
+REM Opcional: mantém a janela aberta após a execução para ver erros ou resultados
+pause
